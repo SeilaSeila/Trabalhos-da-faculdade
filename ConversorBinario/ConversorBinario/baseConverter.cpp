fbe3261d28a471fcf5d709e9a2108076337e4b89
Mateus Gurgel <40184185+SeilaSeila@users.noreply.github.com>
2022-04-19 20:21:12 -0300

    Update baseConverter.cpp

--- ConversorBinario/ConversorBinario/baseConverter.cpp
+++ ConversorBinario/ConversorBinario/baseConverter.cpp
@@ -2,7 +2,7 @@
 
 //there are many ways to do it easily, but I wanted
 //to make a code that converted decimal to hexadecimal 
-//and binary like my teacher did
+//and binary like my professor did
 
 #include <iostream>
 #include <cmath>
